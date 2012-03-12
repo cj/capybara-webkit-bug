c6b3e1f1575169f0fc75045b480557d9c34d052e
cj <cjlazell@googlemail.com>
2012-03-11 23:20:57 -0500

    setting up spec

---
+++ spec/requests/dashboard_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "capybara bug" do
+
+  # before do
+  #   # Capybara.javascript_driver = driver
+  #   # visit root_path
+  # end
+
+  # subject { page }
+
+  # context "#selenium" do
+  #   let(:driver) { :selenium }
+
+  #   it "should fill in the correct date" do
+  #     fill_in 'Datepicker', with: '10/10/2012'
+  #     find_field('Datepicker').value.should equal '10/10/2012'
+  #   end
+
+  # end
+
+  # context "#webkit" do
+  #   let(:driver) { :webkit }
+
+  #   it "should fill in the correct date with the webkit driver" do
+  #     fill_in 'Datepicker', with: '10/10/2012'
+  #     find_field('Datepicker').value.should equal '10/10/2012'
+  #   end
+  # end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,17 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
-
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
-
-Capybara.javascript_driver = :webkit
-
+#
+#
+# # Requires supporting ruby files with custom matchers and macros, etc,
+# # in spec/support/ and its subdirectories.
+# Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+#
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# end
 
 RSpec.configure do |config|
   # ## Mock Framework
@@ -27,12 +23,16 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
