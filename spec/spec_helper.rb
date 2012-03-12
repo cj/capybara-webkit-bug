7d0b9140437063e233dde8a41074ef314156f0b8
cj <cjlazell@googlemail.com>
2012-03-12 09:02:29 -0500

    put back specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,15 +3,15 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
-#
-#
-# # Requires supporting ruby files with custom matchers and macros, etc,
-# # in spec/support/ and its subdirectories.
-# Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-#
-# Capybara.register_driver :selenium do |app|
-#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
-# end
+
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
 
 RSpec.configure do |config|
   # ## Mock Framework
