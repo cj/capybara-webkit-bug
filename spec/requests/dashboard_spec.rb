d5ef6023beab11cdc8e6b4326ee5780aedda7726
cj <cjlazell@googlemail.com>
2012-03-18 12:07:05 -0500

    Changed test description to the same

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -15,7 +15,7 @@ describe "capybara bug" do
   end
 
   context "#webkit", driver: :webkit do
-    it "should fill in the correct date with the webkit driver" do
+    it "should fill in the correct date" do
       fill_in 'First Name', with: 'test'
       fill_in 'Datepicker', with: '10/10/2012'
       find_field('Datepicker').value.should eq '10/10/2012'
